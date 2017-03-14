f1e80b912af7e7108717c34762d7e76610eaf067
Julian Kyer <julian.kyer@gmail.com>
2017-03-14 11:34:53 -0600

    Add district repository data cleaner

--- src/helper.js
+++ src/helper.js
@@ -1,10 +1,32 @@
 export default class DistrictRepository {
   constructor(data) {
-    this.data = data.reduce((acc, curr) => {
-      let location = curr.Location;
-      if(!acc[location]) {
-        
-      } 
+    this.data = data.reduce((mainObj, curr) => {
+      let metrics = {
+        profile: [],
+        location: curr.Location
+      }
+      if(!mainObj[curr.Location]) {
+        mainObj[curr.Location] = metrics;
+        metrics.profile.push({year: curr.TimeFrame, data: curr.Data});
+      } else {
+        mainObj[curr.Location].profile.push({year: curr.TimeFrame, data: curr.Data});
+      }
+      return mainObj;
     }, {});
   }
 }
+
+
+// 
+// 
+// {
+//   colorado: {
+//     data: [{year: 2006, data: 0.5}, {year: 2007, data: 0.4}],
+//     location: colorado
+//   },
+//   boulder: {
+//     data: [{year: 2006, data: 0.5}, {year: 2007, data: 0.4}],
+//     location: boulder
+//   }
+// }
+
