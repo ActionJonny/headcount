87cf3cf492f00b2514758e2e7c8777d1d168d2a5
Julian Kyer <julian.kyer@gmail.com>
2017-03-17 08:19:12 -0600

    Add app test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow, mount, render } from 'enzyme';
 import App from './App';
 
 describe('testing app', () => {
-  it('should ')
+  test('should render an h1, controls, and grid', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1').length).toBe(1);
+    expect(wrapper.find('Controls').length).toBe(1);
+    expect(wrapper.find('Grid').length).toBe(1);
+  });
 });
